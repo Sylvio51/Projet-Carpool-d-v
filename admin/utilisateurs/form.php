0afec487df2a100f86b317726978ef1fc674c03b
Biancolini Sylvio <sylvio-biancolini@hotmail.fr>
2024-03-13 16:48:46 +0100

    ajout du css dans le form

--- admin/utilisateurs/form.php
+++ admin/utilisateurs/form.php
@@ -53,11 +53,14 @@ if (isset($_GET['id']) && $_GET['id'] > 0) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Connexion</title>
+    <link rel="stylesheet" href="/admin/CSS/syle_form.css">
+        <title>Connexion</title>
 </head>
 
 <body>
-
+    <header class="header">
+        
+    </header>
     <div class="container">
         <h1>Inscription</h1>
         <form action="">
