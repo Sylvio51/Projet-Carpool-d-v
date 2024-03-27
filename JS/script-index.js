dd5f5ad49cb69566a7e8377943964b747a2fb1a2
sebastien pabst <sebpabst27@gmail.com>
2024-03-27 16:53:28 +0100

    Animation de la voiture

---
+++ JS/script-index.js
@@ -0,0 +1,9 @@
+// document.addEventListener('scroll', function() {
+//     var voiture = document.querySelector('#publier_annonce .voiture');
+//     var voiturePosition = voiture.getBoundingClientRect();
+  
+//     // Vérifie si l'élément est visible à l'écran
+//     if (voiturePosition.top < window.innerHeight && voiturePosition.bottom >= 0) {
+//       voiture.classList.add('visible');
+//     }
+//   });
