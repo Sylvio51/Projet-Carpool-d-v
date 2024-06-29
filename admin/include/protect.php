f9864fb01291c05eb09d399483757daa2b6f2e55
Biancolini Sylvio <sylvio-biancolini@hotmail.fr>
2024-06-29 19:27:02 +0200

    remodification

--- admin/include/protect.php
+++ admin/include/protect.php
@@ -1,24 +1,6 @@
 <?php session_start();
 if (!isset($_SESSION['user_connected']) || $_SESSION['user_connected'] != "ok") {
-<<<<<<< HEAD
     header("Location:/admin/login.php");
     exit();
 }
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
-=======
-    header("Location:/index.html");
-    exit();
-}
-?>
->>>>>>> 3d59716b681744a114563079b4c4a890097157aa
+
